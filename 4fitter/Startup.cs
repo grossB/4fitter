1670e44196a282f26a9d804e0917425f68134889
Adam Żuk <adam.zuk@outlook.com>
2017-10-22 22:47:19 +0200

    Added project skeleton

---
+++ 4fitter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4fitter.Startup))]
+namespace _4fitter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
