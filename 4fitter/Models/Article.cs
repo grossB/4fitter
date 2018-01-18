227d3ac3ec60cd5ce84663f1f9d4c674ade7ec66
Adam Żuk <adam.zuk@outlook.com>
2018-01-18 21:24:42 +0100

    Layout fixes

--- 4fitter/Models/Article.cs
+++ 4fitter/Models/Article.cs
@@ -10,26 +10,32 @@ namespace _4fitter.Models
         public int ID { get; set; }
 
         [Required]
+        [Display(Name = "Tytuł")]
         public string Title { get; set; }
 
         [Required]
+        [Display(Name = "Adres URL ilustracji")]
         public string IllustrationURL { get; set; }
 
         [Required]
+        [Display(Name = "Łatwy identyfikator")]
         public string FriendlyID { get; set; }
 
         [Required]
+        [Display(Name = "Typ artykułu")]
         public ArticleTypeEnum ArticleType { get; set; }
 
         [Required]
         public string ContentTextFormatted { get; set; }
 
+        [Display(Name = "Autor")]
         public string Author { get; set; }
 
         [Required]
         [NotMapped]
         public string RawTags { get; set; }
 
+        [Display(Name = "Tagi")]
         public virtual ICollection<Tag> Tags { get; set; }
     }
 }
